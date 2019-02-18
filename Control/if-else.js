9c4079c5032d39ba36a29eb6c43becb56a501fa5
Sajan Kumar. Sahu <sahusajankumar@gmail.com>
2019-02-18 19:16:00 +0530

    Delete the deleted files

--- Control/if-else.js
+++
@@ -1,4 +0,0 @@
-let i=10;
-if(i>5){
-    console.log('Greater than 5');
-}
--- Control/landscape_portrait.js
+++
@@ -1,9 +0,0 @@
-
-let landScape = isLandScape(100,50);
-console.log("Printing between Lanscape and Portarait");
-console.log(landScape);
-
-
-function isLandScape(width,height){
-    return width > height ? true : false;
-}
